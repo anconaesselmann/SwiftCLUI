e5323fc07386007df37943c76059f343b906d488
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-06-24 16:27:41 -0700

    Key is custom string convertable

--- Sources/SwiftCLUI/KeyPressEvents/Key.swift
+++ Sources/SwiftCLUI/KeyPressEvents/Key.swift
@@ -3,7 +3,7 @@
 
 import Foundation
 
-public enum Key: Equatable {
+public enum Key: Equatable, CustomStringConvertible {
     case char(Character), up, down, right, left, end, home, insert, delete, pageUp, pageDown, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12
 
     public var character: Character? {
@@ -28,5 +28,33 @@ public enum Key: Equatable {
             return nil
         }
         return NonPrintableCharacter(character)
+    }
+
+    public var description: String {
+        switch self {
+        case .char(let char): return String(char)
+        case .up: return "up"
+        case .down: return "down"
+        case .right: return "right"
+        case .left: return "left"
+        case .end: return "end"
+        case .home: return "home"
+        case .insert: return "insert"
+        case .delete: return "delete"
+        case .pageUp: return "pageUp"
+        case .pageDown: return "pageDown"
+        case .f1: return "f1"
+        case .f2: return "f2"
+        case .f3: return "f3"
+        case .f4: return "f4"
+        case .f5: return "f5"
+        case .f6: return "f6"
+        case .f7: return "f7"
+        case .f8: return "f8"
+        case .f9: return "f9"
+        case .f10: return "f10"
+        case .f11: return "f11"
+        case .f12: return "f12"
+        }
     }
 }
