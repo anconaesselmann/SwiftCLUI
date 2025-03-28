ceea546281a9a93267cbbda0c05ec9bdce99f893
Axel Ancona Esselmann <axel.esselmann@ouraring.com>
2025-03-28 10:46:34 -0700

    AppEvent isRunning property

--- Sources/SwiftCLUI/Environment.swift
+++ Sources/SwiftCLUI/Environment.swift
@@ -8,6 +8,14 @@ public class Environment {
 
     public enum AppEvent {
         case success, running, value(String)
+        
+        var isRunning: Bool {
+            switch self {
+            case .running: return true
+            case .success: return false
+            case .value: return false
+            }
+        }
     }
 
     private init() {}
@@ -23,4 +31,14 @@ internal class Subscriptions {
     static let shared = Subscriptions()
 
     var values: [UUID: AnyCancellable] = [:]
+}
+
+
+extension Result where Success == Environment.AppEvent {
+    var isRunning: Bool {
+        switch self {
+        case .success(let event): return event.isRunning
+        case .failure: return false
+        }
+    }
 }
