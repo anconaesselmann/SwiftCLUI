91a92ebdd4e385fb9f53c971643dd1b9a3d69f6b
Axel Ancona Esselmann <axel.esselmann@ouraring.com>
2025-03-27 22:44:17 -0700

    Initial commit of all supported text styles

--- Sources/SwiftCLUI/View/Modifiers/ForegroundStyles/Strike.swift
+++ Sources/SwiftCLUI/View/Modifiers/ForegroundStyles/Strike.swift
@@ -3,10 +3,74 @@
 
 import Foundation
 
+public struct NoForegroundStyle: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.normal
+    }
+}
+
 public struct Strike: ForegroundStyle {
     public init() { }
     
     public func applied(to content: String) -> String {
         content.strike
+    }
+}
+public struct Bold: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.bold
+    }
+}
+public struct Dim: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.dim
+    }
+}
+public struct Italic: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.italic
+    }
+}
+public struct Underline: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.underline
+    }
+}
+public struct blink: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.blink
+    }
+}
+public struct Overline: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.overline
+    }
+}
+public struct Inverse: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.inverse
+    }
+}
+public struct Hidden: ForegroundStyle {
+    public init() { }
+    
+    public func applied(to content: String) -> String {
+        content.hidden
     }
 }
